351931e286f0fe199773ed3444306f9766043d9d
Aleksey Havronichev <havronichev19alekseyy@gmail.com>
2025-02-13 16:24:26 +0300

    Initial commit

---
+++ src/test/java/com/JavaSpringBoot/MyProject/MyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaSpringBoot.MyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
